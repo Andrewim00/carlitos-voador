aa64f6538b31aeb016f7b03f1c1a5053f44f2a4a
Andrewim00 <163583880+Andrewim00@users.noreply.github.com>
2025-03-14 19:45:38 +0000

    aula 4

--- src/main/java/application/controller/IndexController.java
+++ src/main/java/application/controller/IndexController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class IndexController {
 
-    @RequestMapping("/")
+    //@RequestMapping("/")
     public String home() {
         return "index";
     }
